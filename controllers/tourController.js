2f738a6926acc6348b79da7f7a7a957eb12a8b50
Bipin <bipinyadav9769@gmail.com>
2022-10-29 20:04:57 +0530

    Update atour and delete tour implemented

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -64,32 +64,41 @@ exports.getTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  // if(req.params.id*1>tours.length){
-  //     return res.status(404).json({
-  //         status:"failed",
-  //         message:"Invalid Id"
-  //     })
-  // }
+exports.updateTour = async (req, res) => {
+  
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id,req.body,{
+      new: true,
+      runValidators: true
+    })
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      status:"fail",
+      message:error
+    })
+  }
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<updated tour here .... />>"
-    }
-  });
 };
 
-exports.deleteTour = (req, res) => {
-  // if(req.params.id*1>tours.length){
-  //     return res.status(404).json({
-  //         status:"failed",
-  //         message:"Invalid Id"
-  //     })
-  // }
+exports.deleteTour = async (req, res) => {
+  
+  try {
+    const tour =  await Tour.findByIdAndDelete(req.params.id)
+    res.status(204).json({
+      status: "success",
+      data: tour
+    });
+  } catch (error) {
+    res.status(404).json({
+      status:"fail",
+      message:error
+    })
+  }
 
-  res.status(204).json({
-    status: "success",
-    data: null
-  });
 };
