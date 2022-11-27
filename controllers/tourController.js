ad9f87a839ea536f127be6124c528275c1832d6f
Bipin <bipinyadav9769@gmail.com>
2022-11-27 22:45:09 +0530

    Modelling the Tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -41,6 +41,7 @@ exports.createTour = async (req, res) => {
   res.status(400).json({
     status:"fail",
     message:"Invalid data sent"
+    // message:err
   })
 }
 } 
--- models/tourModel.js
+++ models/tourModel.js
@@ -6,6 +6,27 @@ const tourSchema = new mongoose.Schema({
     required: [true, "A Tour must have a name"],
     unique: true
   },
+  duration :{
+    type : Number ,
+    required: [true , "A Tour must have a duration"]
+
+  },
+  maxGroupSize: {
+    type: Number ,
+    required: [true , "A tour must have a group size"]
+  },
+  difficulty: {
+    type: String ,
+    required : [true , "A tour must have a difficulty"]
+  },
+  ratingsAverage: {
+    type: Number ,
+    default: 4.5
+  },
+  raingsQuantity: {
+    type: Number ,
+    default:0
+  },
   rating: {
     type: Number,
     default: 4.5
@@ -13,7 +34,27 @@ const tourSchema = new mongoose.Schema({
   price: {
     type: Number,
     required: [true, "A tour must have a price"]
-  }
+  },
+  priceDiscount : Number ,
+  summary:{
+    type: String,
+    trim : true,
+    required : [true , "A tour must have a description"]
+  },
+  description : {
+    type : String ,
+    trim :true,
+  },
+  imageCover: {
+    type: String ,
+    required :[true ,"A tour must have image cover"]
+  },
+  images:[String],
+  createdAt: {
+    type: Date ,
+    default : Date.now()
+  },
+  startDates:[Date]
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
