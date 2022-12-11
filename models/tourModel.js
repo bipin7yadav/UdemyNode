ea65ae31ee965668d206336abc0f093270c18670
Bipin <bipinyadav9769@gmail.com>
2022-12-11 22:08:38 +0530

    Built in validation and cutom validation learnt

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,11 +2,17 @@ const mongoose = require("mongoose");
 
 const slugify = require("slugify")
 
+const validator = require('validator')
+
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "A Tour must have a name"],
-    unique: true
+    unique: true,
+    trim :true,
+    maxlength:[40,"A Tour must have less or equal then 40 characters"],
+    minlength:[10, "Atour must have more or equal then 10 characters"],
+    // validate: [validator.isAlpha,"Tour Name must only contain characters"]
   },
   slug:String,
   duration :{
@@ -20,11 +26,17 @@ const tourSchema = new mongoose.Schema({
   },
   difficulty: {
     type: String ,
-    required : [true , "A tour must have a difficulty"]
+    required : [true , "A tour must have a difficulty"],
+    enum: {
+      values :['easy',"medium","difficult"],
+      message :'Difficulty is either: easy , medium , difficult'
+    }
   },
   ratingsAverage: {
     type: Number ,
-    default: 4.5
+    default: 4.5,
+    min:[1,"Rating must  be above 1.0"],
+    max:[5,"Rating must be below 5.0"]
   },
   raingsQuantity: {
     type: Number ,
@@ -38,7 +50,16 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, "A tour must have a price"]
   },
-  priceDiscount : Number ,
+  priceDiscount : {
+    type: Number,
+    validate:{
+      validator: function(val){
+        // this only points to current doc on NEW document creation
+        return val<this.price
+    },
+    message:"Discount price ({VALUE}) should be below regular price"
+  }
+  } ,
   summary:{
     type: String,
     trim : true,
