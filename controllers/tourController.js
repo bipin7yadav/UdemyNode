72f897a4a77b5b0c5ff1a868a1a08f6870ff1d4c
Bipin <bipinyadav9769@gmail.com>
2022-12-26 22:39:43 +0530

    Errors during development vs production

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,6 +5,8 @@ const Tour = require("./../models/tourModel");
 const APIFeatures = require("./../utils/apiFeatures")
 
 const catchAsync = require("./../utils/catchAsync")
+
+const AppError = require("./../utils/appError")
 // Route Handlers
 
 
@@ -126,6 +128,9 @@ exports.getTour = catchAsync(async (req, res, next) => {
     const tour = await Tour.findById(req.params.id)
     // Tour.findOne({_id:req.params.id})
     
+    if(!tour){
+      return next(new AppError('No tour found with that ID',404))
+    }
     res.status(200).json({
       status: "success",
       data: {
@@ -141,6 +146,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
       new: true,
       runValidators: true
     })
+
+    if(!tour){
+      return next(new AppError('No tour found with that ID',404))
+    }
     res.status(200).json({
       status: "success",
       data: {
@@ -154,6 +163,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 exports.deleteTour = catchAsync(async (req, res, next) => {
 
     const tour =  await Tour.findByIdAndDelete(req.params.id)
+
+    if(!tour){
+      return next(new AppError('No tour found with that ID',404))
+    }
     res.status(204).json({
       status: "success",
       data: tour
