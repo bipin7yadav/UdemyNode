bee1556076e795caf234d7d59831b53956da3079
Bipin <bipinyadav9769@gmail.com>
2023-01-06 20:07:19 +0530

    Managing passwords , auth with JWT , Signing up users

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require("bcryptjs")
 
 
 const userSchema =  new mongoose.Schema({
@@ -22,11 +23,30 @@ const userSchema =  new mongoose.Schema({
     },
     passwordConfirm: {
         type : String,
-        required:[true,"Please confirm your password"]
+        required:[true,"Please confirm your password"],
+        validate: {
+            // This only works on Create and SAve
+            validator: function(el){
+                return el=== this.password
+            },
+        message:"Passwords are not the same"
+        }
     }
 
 })
 
+userSchema.pre('save',async function(next){
+
+    // Only run this function if paword was actually modified
+    if(!this.isModified('password')) return next();
+
+    // Hash the password with the cost of 12 
+
+    this.password = await bcrypt.hash(this.password,12)
+
+    // deleting confirm password field
+    this.passwordConfirm = undefined
+})
 const User = mongoose.model('User',userSchema)
 
 module.exports = User
