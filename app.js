4846d0a8f171c1479ce913220cc70325fc606b91
Bipin <bipinyadav9769@gmail.com>
2023-01-21 10:54:28 +0530

    Preventing Parameter pollution

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const rateLimit = require('express-rate-limit')
 const helmet = require("helmet")
 const mongoSanitize = require("express-mongo-sanitize")
 const xss = require("xss-clean")
-
+const hpp = require("hpp")
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorCntroller')
 const tourRouter = require("./routes/tourRoute");
@@ -44,6 +44,13 @@ app.use(mongoSanitize())
 
 // Data Sanitization against XSS
 app.use(xss())
+
+//Prevent Parameter Pollution
+app.use(
+  hpp({
+    whitelist:["duration","ratingsQuantity","ratingsAverage","maxGroupSize","difficulty","price"]
+  })
+  )
 
 //Serving static files
 app.use(express.static(`${__dirname}/public`));
