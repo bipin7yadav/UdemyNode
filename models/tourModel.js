a4478002f960d0e79d844d150c2f20148362dd8d
Bipin <bipinyadav9769@gmail.com>
2023-02-04 13:47:23 +0530

    Modelling and populating tour guide

--- models/tourModel.js
+++ models/tourModel.js
@@ -4,6 +4,8 @@ const slugify = require("slugify")
 
 const validator = require('validator')
 
+// const User = require("./userModel")
+
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -108,6 +110,12 @@ const tourSchema = new mongoose.Schema({
       day: Number
     }
   ],
+  guides: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User'
+    }
+  ]
 },
 {
   toJSON: {virtuals:true},
@@ -127,6 +135,12 @@ tourSchema.pre('save',function( next){
   next()
 })
 
+// tourSchema.pre('save', async function(next) {
+//   const guidesPromises = this.guides.map(async id => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
+
 // tourSchema.pre('save', function(next){
 //   console.log("Will save document...")
 //   next()
@@ -144,6 +158,15 @@ tourSchema.pre(/^find/,function(next){
   this.start = Date.now()
   next()
 })
+
+tourSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  });
+
+  next();
+});
 
 tourSchema.post(/^find/,function(docs,next){
   console.log(`Query took ${Date.now()-this.start} milliseconds`)
