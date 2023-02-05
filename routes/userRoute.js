f5771db79ad0f383254738f3edaac15f5a62cf01
Bipin <bipinyadav9769@gmail.com>
2023-02-05 21:41:35 +0530

    adding a/m endpoint to get current user

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -16,6 +16,7 @@ router.patch('/updateMyPassword',authController.protect, authController.updatePa
 router.patch('/updateMe',authController.protect, userController.updateMe)
 
 router.delete('/deleteMe',authController.protect, userController.deleteMe)
+router.get('/me',authController.protect, userController.getMe, userController.getUser);
 
 router
   .route('/')
