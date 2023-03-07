6f74d2080739633f288bba7e75dc921c333ffa06
Bipin <bipinyadav9769@gmail.com>
2023-03-07 21:47:05 +0530

    fixed minor bug

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -20,7 +20,7 @@ const router = express.Router();
 // router.get('/tour/:slug',viewsController.getTour);
 // router.get('/login',  viewsController.getLoginForm);
 // router.get('/me',authController.protect, viewsController.getAccount);
-router.get('/',bookingController.createBooking, authController.isLoggedIn, viewsController.getOverview);
+router.get('/',authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
